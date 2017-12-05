b972948a2003c77127afe40ade1b7f8f9fe5e833
Eric Clack <eric@bn7.net>
2017-12-05 12:40:46 +0000

    Added first example

---
+++ examples/flowers-or-butterflies.rkt
@@ -0,0 +1,26 @@
+#lang s-exp stamps/lang 
+
+(define (wobble w a)
+  ;; vary width w by up to 20% using sin and a
+  (+ w (* (* w .2) (sin (/ a 4)))))
+
+(define-shape circle
+  ((lines 0) [alpha -.9]
+             [sat 1]
+             [brightness .5]))
+
+(define-shape (lines i)
+  ; draw lines around rotated around centre point
+  ; i is a simple counter for input into wobble
+  (square [s (wobble 100 i) 5])
+  ((lines (add1 i)) [r (rr 0 5)]))
+
+(define-shape scene
+  (circle [h 30])
+  (scene [t (ri -160 160)]
+         [h (ri 0 60)]))
+
+(background '(0 0 0))
+; change cycles to draw more or less circles
+(maximum-render-cycles 10000)
+(start-shape scene)
