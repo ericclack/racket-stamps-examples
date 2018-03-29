06457a920b565b8a7fa51e348f0c383615be40ae
Eric Clack <eric@bn7.net>
2018-03-29 14:37:43 +0100

    Better colours

--- examples/tiling/tiling-sheared-circles1.rkt
+++ examples/tiling/tiling-sheared-circles1.rkt
@@ -1,20 +1,18 @@
 
 #lang s-exp stamps/lang
 
-
-
 (define-shape line-of-circles
-  (circle [shear (rr -.3 .3)])
-  (line-of-circles [x .95]
+  (circle [shear (rr -.25 .25)])
+  (line-of-circles [x .93]
                    [h 15])
 )
 
 (define-shape grid
-  (line-of-circles [sat .4] [b .5])
-  (grid [y .95]
+  (line-of-circles [sat .4] [b .4])
+  (grid [y .93]
         [hue 5]))
 
 (maximum-render-cycles 1000)
-(background '(0 1 1))
+(background '(0 0 .9))
 (bounding '(-15 -12 0.50 0.50))
 (start-shape grid)
